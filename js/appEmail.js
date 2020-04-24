d0bd59572f598dbcfff375efdd2d7880b8e94223
Jose Moreno Gomez <joseutp14@gmail.com>
2020-04-24 17:25:38 -0600

    Imprimiendo el Spinner y Gif de enviado

--- js/appEmail.js
+++ js/appEmail.js
@@ -3,10 +3,11 @@
 //Branch con GitKraken
 
 const email = document.getElementById('email');
-const asunto =document.getElementById('asunto');
+const asunto = document.getElementById('asunto');
 const mensaje = document.getElementById('mensaje');
 const btnEnviar = document.getElementById('enviar');
-
+const formularioEnviar = document.getElementById('enviar-mail');//Otra forma de enviar email usando ID del campo
+const resetBtn = document.getElementById('resetBtn')
 //Event Listener
 eventListeners();
 
@@ -14,17 +15,16 @@ function eventListeners(){
 
      //Inicio de la aplicacion y desabilitar Submit
      document.addEventListener('DOMContentLoaded', inicioApp);
-
-     //Campos del formulario
-     email.addEventListener('blur',validarCampo);
-     asunto.addEventListener('blur',validarCampo);
-     mensaje.addEventListener('blur',validarCampo);
+     email.addEventListener('blur', validarCampo);
+     asunto.addEventListener('blur', validarCampo);
+     mensaje.addEventListener('blur', validarCampo);
+     btnEnviar.addEventListener('click', enviarEmail);
+     resetBtn.addEventListener('click',resetFormulario);
 }
 
 //Functions
 function inicioApp(){
      //Desabilitar el envio
-
      btnEnviar.disabled = true;
 }
 
@@ -36,9 +36,7 @@ function validarCampo(){
 
      //Validar unicamente el Email(@)
      if(this.type === 'email'){
-
           validarEmail(this);
-
      }
      let errores = document.querySelectorAll('.error');
 
@@ -49,6 +47,45 @@ function validarCampo(){
      }
 }
 
+//Resetear formulario
+
+function resetFormulario(){
+
+     formularioEnviar.reset();
+
+     e.preventDefault();
+
+}
+
+//Cuando se envia el correo
+
+function enviarEmail(e){
+     //Spinner al presionar enviar
+     const spinnerGif =document.querySelector('#spinner');
+     spinnerGif.style.display = 'block';
+
+     //Gif que envia email
+     const enviado = document.createElement('img');
+     enviado.src = 'img/mail.gif'
+     enviado.style.display = 'block';
+
+     //ocultar spinner y mostrar gif de enviado
+
+     setTimeout(function(){
+          spinnerGif.style.display = 'none';
+
+          document.querySelector('#loaders').appendChild( enviado );
+
+          setTimeout(function(){
+               enviado.remove();
+               formularioEnviar.reset();
+          },5000);
+     }, 3000);
+
+     e.preventDefault();
+}
+
+//Verifica la longitud del texto en los campos
 function validarLongitud(campo){
      
 
@@ -73,3 +110,4 @@ function validarEmail(campo){
      }
 
 }
+
