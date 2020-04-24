b11c9fa44352543e07eb56352c6adc00e9df6b48
Jose Moreno Gomez <joseutp14@gmail.com>
2020-04-24 15:40:29 -0600

    Se desabilito el boton de enviar cuando los campos estan vacios

--- js/appEmail.js
+++ js/appEmail.js
@@ -1,3 +1,27 @@
 //Simulador de envio de Email utilizando JavaScript 
 //Jose Moreno Gomez
 //Branch con GitKraken
+
+const email = document.getElementById('email');
+const asunto =document.getElementById('asunto');
+const mensaje = document.getElementById('mensaje');
+const btnEnviar = document.getElementById('enviar');
+
+//Event Listener
+eventListeners();
+
+function eventListeners(){
+
+     //Inicio de la aplicacion y desabilitar Submit
+     document.addEventListener('DOMContentLoaded', inicioApp);
+}
+
+
+
+
+//Functions
+function inicioApp(){
+     //Desabilitar el envio
+
+     btnEnviar.disabled = true;
+}
